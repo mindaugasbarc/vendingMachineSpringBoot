64137b520b4c599c9c4c527b2849ee4f5fb9ed8c
Mindaugas <mindaugas.barcauskas@mif.stud.vu.lt>
2019-05-08 20:27:05 +0300

    adding exception handler

---
+++ src/main/java/com/mindaugasb/vending/machine/exception/handler/VendingMachineExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.mindaugasb.vending.machine.exception.handler;
+
+import com.mindaugasb.vending.machine.exception.InsufficientBalanceException;
+import com.mindaugasb.vending.machine.exception.ItemNotFoundException;
+import com.mindaugasb.vending.machine.exception.OutOfStockException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class VendingMachineExceptionHandler {
+
+    @ExceptionHandler(value = InsufficientBalanceException.class)
+    public ResponseEntity<String> handleInsufficientBalanceException(InsufficientBalanceException exception) {
+        return new ResponseEntity<>("you do not hauve enough money to buy this, please insert coins",
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = ItemNotFoundException.class)
+    public ResponseEntity<String> handleItemNotFoundException(ItemNotFoundException exception) {
+        return new ResponseEntity<>("Item not found, please try different code",
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(value = OutOfStockException.class)
+    public ResponseEntity<String> handleOutOfStockException(OutOfStockException exception) {
+        return new ResponseEntity<>("item is out of stock, please choose another one",
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
--- src/main/java/com/mindaugasb/vending/machine/service/VendingService.java
+++ src/main/java/com/mindaugasb/vending/machine/service/VendingService.java
@@ -3,7 +3,6 @@ package com.mindaugasb.vending.machine.service;
 import com.mindaugasb.vending.machine.model.Item;
 
 import java.util.List;
-import java.util.Set;
 
 public interface VendingService {
 
